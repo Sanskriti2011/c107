3572597229e223a2ae8a809786a86f980ad68866
Sanskriti2011 <87705557+Sanskriti2011@users.noreply.github.com>
2022-08-31 19:59:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+     var Content = event.results[0][0].transcript;
+     console.log(Content);
+     document.getElementById("textbox").innerHTML = Content;
+     speak();
+     
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "taking your selfie";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    console.log("speak_data")
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
